3deec9c89d760855b8c4a06deb17945d8d655629
Nathan Peek <nmp14fsu@gmail.com>
2021-05-03 19:39:42 -0400

    Add pages and some default styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter as RouterisCool, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
 
 function App(props) {
-    render(
+    return (
         <div>
-
+            <RouterisCool>
+                <Wrapper>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/Resume" component={Resume} />
+                </Wrapper>
+            </RouterisCool>
         </div>
     )
 }
