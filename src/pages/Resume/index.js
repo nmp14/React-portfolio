271cded2d07e1e60108a2238e3180daec7cb8cf2
Nathan Peek <nmp14fsu@gmail.com>
2021-05-03 20:06:36 -0400

    Add resume to page with styling

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Header from "../../components/Header";
+import "./style.css";
 
-class Resume extends React.Component {
-
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
+function Resume(props) {
+    return (
+        <main>
+            <Header>
+                <nav>
+                    <h2>My Resume</h2>
+                    <p><a href="/">Go Back</a></p>
+                </nav>
+            </Header>
+            <iframe src="https://drive.google.com/file/d/16Kmnugc5iNHDNC-bq64XLkFhbhowrnWW/preview"></iframe>
+        </main>
+    )
 }
 
 export default Resume;
