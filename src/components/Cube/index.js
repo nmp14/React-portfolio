7200d349e03db9da0a531c6c2ba0f81cd509e604
Nathan Peek <nmp14fsu@gmail.com>
2021-05-04 00:21:32 -0400

    Move NavPAge to cube from home

--- src/components/Cube/index.js
+++ src/components/Cube/index.js
@@ -4,24 +4,40 @@ import Home from "../Home";
 import AboutMe from "../AboutMe";
 import Portfolio from "../Portfolio";
 import CubeFaceContext from "../../util/cubeFaceContext";
+import NavPage from "../../components/NavPage";
+import NavPageContext from "../../util/NavPageContext";
 
 function Cube(props) {
     const [cubeFace, setCubeFace] = useState("");
+    // Change whether the navpage displays or not (hidden if true)
+    const [navState, setNavState] = useState(true);
+    const [animateNavPage, setAnimateNavPageState] = useState(false);
 
     const handleCubeFaceChange = (side) => {
         setCubeFace(side);
     }
 
+    const handleNavPageChange = (event) => {
+        event.preventDefault();
+        const newNavState = !navState
+        setNavState(newNavState);
+
+        if (!animateNavPage) setAnimateNavPageState(true);
+    }
+
     return (
         <main className="site-cube">
-            {/* Rotates cube based on class name and state of cube */}
-            <CubeFaceContext.Provider value={{ cubeFace, handleCubeFaceChange }}>
-                <div className={cubeFace ? "cube " + cubeFace : "cube show-front"}>
-                    <Home />
-                    <AboutMe />
-                    <Portfolio />
-                </div>
-            </CubeFaceContext.Provider>
+            <NavPageContext.Provider value={{ navState, handleNavPageChange, animateNavPage }}>
+                {/* Rotates cube based on class name and state of cube */}
+                <CubeFaceContext.Provider value={{ cubeFace, handleCubeFaceChange }}>
+                    <NavPage />
+                    <div className={cubeFace ? "cube " + cubeFace : "cube"}>
+                        <Home />
+                        <AboutMe />
+                        <Portfolio />
+                    </div>
+                </CubeFaceContext.Provider>
+            </NavPageContext.Provider>
         </main>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 import Wrapper from "../../components/Wrapper";
 import Cube from "../../components/Cube";
-import NavPage from "../../components/NavPage";
-import NavPageContext from "../../util/NavPageContext";
 
 function Home(props) {
-    // Change whether the navpage displays or not (hidden if true)
-    const [navState, setNavState] = useState(true);
-    const [animateNavPage, setAnimateNavPageState] = useState(false);
-
-    const handleNavPageChange = (event) => {
-        event.preventDefault();
-        const newNavState = !navState
-        setNavState(newNavState);
-
-        if (!animateNavPage) setAnimateNavPageState(true);
-    }
-
     return (
         <Wrapper>
-            <NavPageContext.Provider value={{ navState, handleNavPageChange, animateNavPage }}>
-                <NavPage />
-                <Cube />
-            </NavPageContext.Provider>
+            <Cube />
         </Wrapper>
     )
 }
