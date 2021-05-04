0bb672f420f7c4a6b7466ee21b72641425dba647
Nathan Peek <nmp14fsu@gmail.com>
2021-05-04 04:27:07 -0400

    Cant figure out gh-pages and react-router

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -6,7 +6,7 @@ function Resume(props) {
         <main className="resume">
             <nav>
                 <h2>My Resume</h2>
-                <p><a className="resumeNav" href="/">Go Back</a></p>
+                <p><a className="resumeNav" href={process.env.PUBLIC_URL + '/'}>Go Back</a></p>
             </nav>
             <iframe src="https://drive.google.com/file/d/16Kmnugc5iNHDNC-bq64XLkFhbhowrnWW/preview" title="Resume PDF"></iframe>
         </main>
