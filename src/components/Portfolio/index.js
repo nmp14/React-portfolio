d183b8c343459c96f22c122e4e89eb58808e8fdd
Nathan Peek <nmp14fsu@gmail.com>
2021-05-28 14:02:38 -0400

    Update images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -55,7 +55,7 @@ function Portfolio() {
                 {/* Code Pub project */}
                 <Card
                     title={"Code Pub"}
-                    url={"Images/Code-Pub-min.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/Code-Pub-min.png`}
                     alt={"Homepage for Code Pub restaurant site"}
                     dataTitle={"code-pub"}
                     getProjectDesc={getProjectDesc}
@@ -64,7 +64,7 @@ function Portfolio() {
                 {/* Moods project */}
                 <Card
                     title={"#Moods"}
-                    url={"Images/moods_page-min.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/moods_page-min.png`}
                     alt={"#Moods app home page. Music social media platform"}
                     dataTitle={"moods"}
                     getProjectDesc={getProjectDesc}
@@ -73,7 +73,7 @@ function Portfolio() {
                 {/* FSU lab site */}
                 <Card
                     title={"Stiegman Lab (FSU)"}
-                    url={"Images/StiegmanLab-min.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/StiegmanLab-min.png`}
                     alt={"#Site for Dr. Stiegman's chemistry lab at FSU"}
                     dataTitle={"stiegman-lab"}
                     getProjectDesc={getProjectDesc}
@@ -82,7 +82,7 @@ function Portfolio() {
                 {/* AniMoves project */}
                 <Card
                     title={"AniMoves"}
-                    url={"Images/animove-min.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/animove-min.png`}
                     alt={"#Homepage for animoves app that keeps track of anime and movies"}
                     dataTitle={"ani-moves"}
                     getProjectDesc={getProjectDesc}
@@ -91,7 +91,7 @@ function Portfolio() {
                 {/* Employee directory react app */}
                 <Card
                     title={"Employee Directory"}
-                    url={"Images/employee-directory-min.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/employee-directory-min.png`}
                     alt={"Directory of employees sortable and filterable by names"}
                     dataTitle={"employee-directory"}
                     getProjectDesc={getProjectDesc}
@@ -100,7 +100,7 @@ function Portfolio() {
                 {/* BlogPosts  */}
                 <Card
                     title={"BlogPosts"}
-                    url={"Images/tech_blog.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/tech_blog.png`}
                     alt={"Blog post app"}
                     dataTitle={"blog-posts"}
                     getProjectDesc={getProjectDesc}
@@ -109,7 +109,7 @@ function Portfolio() {
                 {/*  BudgeGrub */}
                 <Card
                     title={"BudgeGrub"}
-                    url={"Images/BudgeGrub.png"}
+                    url={`${process.env.PUBLIC_URL}/Images/BudgeGrub.png`}
                     alt={"Budge grub app"}
                     dataTitle={"budge-grub"}
                     getProjectDesc={getProjectDesc}
